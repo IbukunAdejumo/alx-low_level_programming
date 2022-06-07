8b200918d70441143336b4030674a6a205c8bda1
IbukunAdejumo <ibukunadejumo@yahoomail.com>
2022-06-07 11:14:39 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
-#insert <stdio.h>
+#include "main.h"
+/**
+ * main - print_alphabet Entry point
+ * Documentation: print_alphabet - prints the alphabet in lower case
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char n;
+
+	n = 'a';
+	while (n <= 'z')
+	{
+		_putchar(n);
+		n++;
+	}
+	-putchar('\n');
+}
