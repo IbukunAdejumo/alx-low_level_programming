cab8c0d0edd55b7e3ae03bd4ac5789eb220ae50c
IbukunAdejumo <ibukunadejumo@yahoomail.com>
2022-06-10 19:02:57 -0700

    Task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-#ifndef MAIN_FILE
 #define MAIN_FILE
 
 int _putchar(char c);
@@ -13,4 +12,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-endif
+endif;
