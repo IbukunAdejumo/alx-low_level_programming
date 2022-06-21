8ae3cb8552ae253d031e4926bbe845c4e6186c90
IbukunAdejumo <ibukunadejumo@yahoomail.com>
2022-06-21 08:19:08 -0700

    a function that sets the value of a pointer to a char

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * _memcpy - copies memory area
- * @src: from memory area src
- * @dest: to memory area dest
+ * char * _memcpy - copies memory area
+ * @dest: destination
+ * @src: source
  * @n: number of bytes to copy
  * Return: pointer to dest
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
 
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	int size = n;
+
+	if (size > 0)
+	{
+		int 1;
 
 	for (i = 0; i < n; i++)
 		*(dest + i) = *(src + i);
