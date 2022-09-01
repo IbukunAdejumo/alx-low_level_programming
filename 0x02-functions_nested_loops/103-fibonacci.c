09da3557be29f9d774e1ccd02424f1fc202b5d28
IbukunAdejumo <ibukunadejumo@yahoomail.com>
2022-09-01 13:38:24 -0700

    Print list

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 /**
- * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * main - finds & prints the sum 
  * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	long int i, j, k, next;
+	int i;
+	unsigned long int j,k next,sum
 
 	j = 1;
-
 	k = 2;
+	sum = 0;
 
-	for (i = 1; i <= 50; ++i)
+	for (i = 1; i <= 33; ++i)
 	{
-		if (j != 20365011074)
+		if (j < 4000000 && (j % 2) == 0
 		{
-			printf("%ld, ", j);
+			sum = sum + j;
 		}
-		else
-		{
-			printf("%ld\n", j);
-		}
-
+		next = j + k;
 		j = k;
 		k = next;
+
+		
 	}
-	return (0);
+	
+	printf("%lu\n", sum);
+		
+		return (0);
 
 }
 
